87695dbd179cae249fc5358491ce01cccb1fe04a
UBC Student <jhan27@pender.students.cs.ubc.ca>
2024-02-04 23:35:47 -0800

    Initial commit

---
+++ src/mr/rpc.go
@@ -0,0 +1,41 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"os"
+	"strconv"
+)
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type RequestVoteArgs struct {
+	Term         int
+	CandidateID  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type RequestVoteReply struct {
+	Term        int
+	VoteGranted bool
+}
+
+type 
+
+// Add your RPC definitions here.
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the coordinator.
+func coordinatorSock() string {
+	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
